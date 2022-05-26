8d58293ce2cef649849f8d4b2510342f38848456
Job van der Zweep <jobvan-d@student.codam.nl>
2022-05-26 13:18:40 +0200

    remove costly sqrt in intersects_sphere

--- src/calculate_intersections/sphere/sphere.c
+++ src/calculate_intersections/sphere/sphere.c
@@ -6,7 +6,7 @@
 /*   By: rnijhuis <rnijhuis@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/25 18:36:15 by rnijhuis      #+#    #+#                 */
-/*   Updated: 2022/05/25 19:32:15 by rnijhuis      ########   odam.nl         */
+/*   Updated: 2022/05/26 13:07:42 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@
 #include <math.h>
 #include <stdbool.h>
 
-static float	get_intersection_point(float radius, float distance_to_so, \
+static float	get_intersection_point(float radius, float distance_to_so_sq, \
 	float distance_to_closest_point)
 {
 	float	intersect_point;
 	float	distance_from_origin_to_ray;
-	float	distance_to_so_sq;
 	float	radius_sq;
 
 	radius_sq = radius * radius;
-	distance_to_so_sq = distance_to_so * distance_to_so;
 	distance_from_origin_to_ray = sqrt(radius_sq - distance_to_so_sq);
 	intersect_point = distance_to_closest_point - distance_from_origin_to_ray;
 	return (intersect_point);
@@ -39,18 +37,18 @@ float	intersects_sphere(t_ray *ray, t_object *shape)
 	t_sphere	*sphere;
 	t_vec3f		relpos;
 	t_vec3f		closest_point;
-	float		distance_to_so;
+	float		distance_to_so_sq;
 	float		distance_to_closest_point;
 
 	sphere = &shape->sphere;
 	relpos = sphere->base.position - ray->origin;
 	distance_to_closest_point = vec3f_dot(relpos, ray->direction);
 	closest_point = ray_at(ray, distance_to_closest_point);
-	distance_to_so = vec3f_length(sphere->base.position - closest_point);
-	if (distance_to_so <= sphere->radius)
+	distance_to_so_sq = vec3f_len_sq(sphere->base.position - closest_point);
+	if (distance_to_so_sq <= sphere->radius * sphere->radius)
 	{
 		return (get_intersection_point(sphere->radius,
-				distance_to_so, distance_to_closest_point));
+				distance_to_so_sq, distance_to_closest_point));
 	}
 	return (-1);
 }
