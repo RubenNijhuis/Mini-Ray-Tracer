b4a471b30ec74c1f8421aad794681447f482e930
Ruben Nijhuis <contact@rubennijhuis.com>
2022-05-28 02:10:54 +0200

    Temp fix stack overflow

--- src/file_parse/is_file_correctly_formatted/shape_settings.c
+++ src/file_parse/is_file_correctly_formatted/shape_settings.c
@@ -6,7 +6,7 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/26 09:55:21 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/05/28 01:56:37 by rubennijhui   ########   odam.nl         */
+/*   Updated: 2022/05/28 02:10:21 by rubennijhui   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ bool	check_rotation(char *settings)
 	char	**split_settings;
 
 	split_settings = ft_split(settings, ',');
-	if (ft_2d_arrlen(split_settings) != 3)
+	if (ft_2d_arrlen(split_settings) != 1)
 		printf("Error: rotation vec formatted incorrectly\n");
 	ft_free_2d_array(&split_settings, ft_2d_arrlen(split_settings));
 	return (true);
@@ -32,7 +32,7 @@ bool	check_position(char *settings)
 	char	**split_settings;
 
 	split_settings = ft_split(settings, ',');
-	if (ft_2d_arrlen(split_settings) != 3)
+	if (ft_2d_arrlen(split_settings) != 1)
 		printf("Error: position vec formatted incorrectly\n");
 	ft_free_2d_array(&split_settings, ft_2d_arrlen(split_settings));
 	return (true);
@@ -43,7 +43,7 @@ bool	check_color(char *settings)
 	char	**split_settings;
 
 	split_settings = ft_split(settings, ',');
-	if (ft_2d_arrlen(split_settings) != 3)
+	if (ft_2d_arrlen(split_settings) != 1)
 		printf("Error: color vec formatted incorrectly\n");
 	ft_free_2d_array(&split_settings, ft_2d_arrlen(split_settings));
 	return (true);
@@ -56,23 +56,20 @@ void	run_object_checks(t_object_type obj_type, char *obj_settings, \
 	const t_component_checker *component_checkers)
 {
 	// uint32_t	current_check;
-	// int	amount_checks;
+	// uint32_t	amount_checks;
 	// int i = 0;
+	// char *split_settings;
 
 	(void)obj_settings;
 	(void)obj_type;
 	(void)object_components;
+	// split_settings = ft_split(obj_settings, ' ');
 	// (void)component_checkers;
 	// i = 0;
-	// amount_checks = object_components[obj_type].amount_obj_components;
+	// amount_checks = object_components[2].amount_obj_components;
 	// amount_checks = 3;
+	// printf("%i", amount_checks);
 	for (int i = 0; i < 3; i++)
-	{
-		if (i == 0 && (*component_checkers[2].func)("WHT,HELLO,BONJOUR") == false)
-			printf("WHAT");
-		if (i == 1 && (*component_checkers[1].func)("WHT,HELLO,BONJOUR") == false)
-			printf("WHAT");
-		// if (i == 2 && (*component_checkers[2].func)("WHT,HELLO,BONJOUR") == false)
-		// 	printf("WHAT");
-	}
+		(*component_checkers[i].func) \
+			("WHT,HELLO,BONJOUR");
 }
