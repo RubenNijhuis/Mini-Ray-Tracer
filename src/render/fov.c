395183dddfbe04c99cfbdb6b598e78cd083d50e8
Job van der Zweep <jobvan-d@student.codam.nl>
2022-07-12 12:45:03 +0200

    camera rotation experiment

--- src/render/fov.c
+++ src/render/fov.c
@@ -6,7 +6,7 @@
 /*   By: jobvan-d <jobvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/30 14:08:16 by jobvan-d      #+#    #+#                 */
-/*   Updated: 2022/07/08 17:42:11 by jobvan-d      ########   odam.nl         */
+/*   Updated: 2022/07/12 12:40:36 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ t_ray	get_camera_ray(uint32_t xpixel, uint32_t ypixel, t_camera *cam)
 	const double	inv_aspect = (double)WIN_HEIGHT / (double)WIN_WIDTH;
 	const float		fov_tan = tan(cam->fov * 0.5f); // TODO: cache this thing, put it into camera
 	t_ray			ray;
+	t_vec3f			old_pos;
 
 	ray.origin = cam->position;
 	ray.direction[0] = fov_tan
@@ -29,8 +30,12 @@ t_ray	get_camera_ray(uint32_t xpixel, uint32_t ypixel, t_camera *cam)
 	ray.direction[1] = (fov_tan * inv_aspect)
 		* (((double)ypixel - (double)(WIN_HEIGHT / 2)) / (double)WIN_HEIGHT);
 	ray.direction[2] = 1;
-	// ray.direction = rotate(rotate_x, ray.direction, deg_to_rad(60));
-	// ray.direction = rotate(rotate_y, ray.direction, deg_to_rad(0));
+
+	old_pos = cam->position;
 	vec3f_normalize(&ray.direction);
+	vec3f_normalize(&cam->rotation);
+	
+	ray_rotate(&ray, vec3f(0, 0, 1), cam->rotation);
+	cam->position = old_pos;
 	return (ray);
 }
