adc9c26c04448b1a2fd075e58ed5008d5f7b9e85
Job van der Zweep <jobvan-d@student.codam.nl>
2022-07-28 16:53:53 +0200

    add comment sq

--- src/utils/math.c
+++ src/utils/math.c
@@ -6,10 +6,11 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/22 16:31:27 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/07/22 16:31:42 by rubennijhui   ########   odam.nl         */
+/*   Updated: 2022/07/28 16:53:38 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* returns n squared */
 float	sq(const float n)
 {
 	return (n * n);
